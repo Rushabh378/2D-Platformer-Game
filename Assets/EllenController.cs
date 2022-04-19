eeb5a64485b385890e5d4010707cf1d3c624a551
Rushabh Gohel <103501258+Rushabh378@users.noreply.github.com>
2022-04-19 13:57:23 +0530

    created a player controller

---
+++ Assets/EllenController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EllenController : MonoBehaviour
+{
+    [SerializeField]
+    float movement_speed = 0f;
+    Animator animator;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement_speed = Input.GetAxis("Horizontal");
+        animator.SetFloat("speed", Mathf.Abs(movement_speed));
+
+        Vector2 scale = transform.localScale;
+        if (movement_speed < 0)
+            scale.x = -1f * Mathf.Abs(scale.x);
+        else if(movement_speed > 0)
+            scale.x = Mathf.Abs(scale.x);
+        transform.localScale = scale;
+    }
+}
