abd1d4cd1d69d8e8c3e360a9be8da02574e32188
Rushabh Gohel <103501258+Rushabh378@users.noreply.github.com>
2022-04-25 13:07:28 +0530

    add a level

---
+++ Assets/MyScript/EndLevel.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndLevel : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            
+            resumeGame();
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SpriteRenderer sprite = collision.gameObject.GetComponent<SpriteRenderer>();
+        if (sprite)
+        {
+            sprite.enabled = false;
+            Debug.Log("Level Finished");
+            pauseGame();
+        }
+    }
+
+    void pauseGame()
+    {
+        Time.timeScale = 0;
+    }
+    void resumeGame()
+    {
+        Time.timeScale = 1;
+    }
+}
