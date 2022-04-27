97a5394dcae272b6750bdc9d3db94be127d8a14d
Rushabh Gohel <103501258+Rushabh378@users.noreply.github.com>
2022-04-27 13:15:08 +0530

    added key collection

---
+++ Assets/MyScript/Equipment.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Equipment : MonoBehaviour
+{
+    public Image KeyUI;
+    Animator KeyAnime;
+    private void Start()
+    {
+        KeyAnime = KeyUI.GetComponent<Animator>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Collectable"))
+        {
+            KeyAnime.SetBool("key_collected", true);
+            Destroy(collision.gameObject);
+        }
+    }
+}
